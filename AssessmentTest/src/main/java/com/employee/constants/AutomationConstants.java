1b317c44c7fa0fbac51344d76fd4ab5c2c67fb88
JubyJoseph2020 <74653069+JubyJoseph2020@users.noreply.github.com>
2022-12-16 08:04:30 +0530

    Add files via upload

---
+++ AssessmentTest/src/main/java/com/employee/constants/AutomationConstants.java
@@ -0,0 +1,20 @@
+package com.employee.constants;
+
+public class AutomationConstants {
+	
+	public static String expusername="practice.swtesting@gmail.com";
+	public static String exppassword="123";
+	public static String ExpTitle=":: ICT ACADEMY OF KERALA ::";
+	public static String expname="Anu S";
+	public static String exppwd="123";
+	public static String expemail="anu123@gmail.com";
+	public static String expdesignation="HR";
+	public static String expreporting="Demo";
+	public static String expmember="HR";
+	public static String expempid="2345";
+	public static String expconfirmpwd="123";
+	public static String expmobileno="9876543210";
+	public static String expemptype="Temp";
+	public static String expaddress="abcd(h), kottayam";
+
+}
